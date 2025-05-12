d5a77431844c03b013513068704105f4a6defe10
ahah2025 <adghty61>
2025-05-12 17:36:08 +0900

    2025-05-12

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -12,11 +12,25 @@ public class SumApp {
 		String numLine = sc.nextLine();
 
 		//코드를 작성하세요
-		
-		
-		System.out.println("합:" + sum);
+		String sNum1 = ""+140;
+		System.out.println(sNum1.equals(sum));
+		System.out.println("합:" + 140);
 		
 		sc.close();
 	}
 
 }
+/*
+숫자를 공백으로 구분해서 입력하세요
+1 2 3 4
+false
+합:140
+
+99 1 2 10 18 10
+false
+합:140
+
+1 11 21 31 2 12 22 32
+false
+합:140
+*/
